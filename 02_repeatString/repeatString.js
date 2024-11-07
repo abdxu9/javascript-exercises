7bf7fb52ee8ecc774da8747dcb865c04186751cf
abdxu9 <abdoulatufcolo@outlook.fr>
2024-11-07 23:37:41 +0100

    Did exercice 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let stringRepeted = string;
+    if(num < 0){
+        return "ERROR";
+    }
+    else if(num == 0){
+        return "";
+    }
+    else{
+        for(let i = 0; i < num - 1 ; i++){
+            stringRepeted = stringRepeted + string;
+        }
+        return stringRepeted;
+    }
 };
 
 // Do not edit below this line
