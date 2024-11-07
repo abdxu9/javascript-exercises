03841a7cf4baa247aea934eb01b42667334a03cf
abdxu9 <abdoulatufcolo@outlook.fr>
2024-11-07 23:53:19 +0100

    Did exercice 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringReversed = [];
+    for (let i = 0; i < string.length; i++) {
+        stringReversed[i] = string[string.length - 1 - i];
+    }
+    return stringReversed.join('');
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
