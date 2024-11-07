9bfa8caf210036aa57d7d1a7e12d1439acdc67ae
abdxu9 <abdoulatufcolo@outlook.fr>
2024-11-08 00:47:18 +0100

    Did exercice 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,8 +1,6 @@
 const reverseString = function(string) {
     let stringReversed = [];
-    for (let i = 0; i < string.length; i++) {
-        stringReversed[i] = string[string.length - 1 - i];
-    }
+    string.for
     return stringReversed.join('');
 };
 
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    let newArray = [];
+    const argsToRemove = [...arguments].slice(1);
+
+    array.forEach((element) => {
+        if(!argsToRemove.includes(element)){
+             newArray.push(element);
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
