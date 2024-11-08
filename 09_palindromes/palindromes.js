aafdb38ba4279d381b7115a7f886b6093af1f2a6
abdxu9 <abdoulatufcolo@outlook.fr>
2024-11-08 18:32:42 +0100

    Did exercice 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const stringNormalized = string.toLowerCase().split("")
+    .filter((character) => alphanumerical.includes(character))
+    .join("");
 
+    const stringReversed = stringNormalized.split("").reverse().join("");
+
+    return (stringReversed === stringNormalized);
 };
 
 // Do not edit below this line
