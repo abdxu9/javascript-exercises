45f2378b8b6954767543be18271a2dbf1b9c8c2c
abdxu9 <abdoulatufcolo@outlook.fr>
2024-11-08 18:11:11 +0100

    Did exercice 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(firstNumber, secondeNumber) {
+	return firstNumber + secondeNumber;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNumber, secondeNumber) {
+	return firstNumber - secondeNumber;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  elementSum = 0; 
+  while(array.length !== 0){
+    elementSum += array[array.length - 1];
+    array.pop();
+  }
+  return elementSum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  elementMultiply = 1; 
+  while(array.length !== 0){
+    elementMultiply = elementMultiply * array[array.length - 1];
+    array.pop();
+  }
+  return elementMultiply;
 };
 
-const power = function() {
-	
+const power = function(numberToPower, power) {
+	return Math.pow(numberToPower,power);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let factorialOfNumber = 1;
+  for(let i = 1; i <= number; i++){
+    factorialOfNumber*=i
+  }
+  return factorialOfNumber;
 };
 
 // Do not edit below this line
