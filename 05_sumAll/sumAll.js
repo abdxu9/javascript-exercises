ef6e471b4735eb8c73c838c45fc6c9e627dc7283
abdxu9 <abdoulatufcolo@outlook.fr>
2024-11-08 10:40:21 +0100

    Did exercice 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,33 @@
-const sumAll = function() {
+const sumAll = function(intOne, intTwo) {
+    let validArgs = (Number.isInteger(intOne)) && (Number.isInteger(intTwo));
+    let positiveArgs = (intOne > 0) && (intTwo > 0);
+    let oneInfTwo = intOne <= intTwo;
+
+    if(positiveArgs && validArgs){
+        let intSum = 0;
+
+        if(oneInfTwo){
+            let i = intOne;
+            while(i !== (intTwo + 1) ){
+                intSum += i ;
+                i++;
+            }
+            return intSum;
+        }
+        else{
+            let i = intTwo;
+
+            while(i !== (intOne + 1) ){
+                intSum += i ;
+                i++;
+            }
+            return intSum;
+        }  
+    }
+
+    else{
+        return "ERROR";
+    }
 
 };
 
