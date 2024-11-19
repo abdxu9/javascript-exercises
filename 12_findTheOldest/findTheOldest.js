52e1d042f1478e284c8b858427d7e16359e80df3
abdxu9 <abdou@Xubuntu.myguest.virtualbox.org>
2024-11-19 23:33:02 +0100

    Did exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,36 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(Persons) {
+    let agePersonne = Persons.map((element) => {
+        if (element.hasOwnProperty("yearOfDeath")){
+            return element.yearOfDeath - element.yearOfBirth
+        }
+        else{
+            const currentTime = new Date()
+            return currentTime.getFullYear() - element.yearOfBirth
+        }
+        
+    });
+    index = agePersonne.findIndex((element) => element == Math.max(...agePersonne));
+    console.log(index);
+    return Persons[index];
 };
 
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+findTheOldest(people);
 // Do not edit below this line
 module.exports = findTheOldest;
