9978b2df198437aa1cfdcf57c1fc672280de94af
abdxu9 <abdou@Xubuntu.myguest.virtualbox.org>
2024-11-19 22:48:43 +0100

    Did exercice 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,13 @@
-const getTheTitles = function() {
+const getTheTitles = function(bookArray) {
+    /* SOLUTION 1
+    let bookTitles=[];
+    bookArray.forEach((element) => {
+        bookTitles.push(element.title);       
+    });
+    return bookTitles; */
 
+    /* SOLUTION 2 */
+    return bookArray.map((book)=>book.title);
 };
 
 // Do not edit below this line
